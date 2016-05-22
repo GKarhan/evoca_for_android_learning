0c3bafa67f2ccc0bd258625130ca7935fc136a9a
Vache <vmumchyan@gmail.com>
2016-05-23 00:15:09 +0400

    logo

--- EvocaForAndroidLearning/app/src/main/java/com/example/evoca/evocaforandroidlearning/MainActivity.java
+++ EvocaForAndroidLearning/app/src/main/java/com/example/evoca/evocaforandroidlearning/MainActivity.java
@@ -5,8 +5,11 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ExpandableListView;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
 
@@ -17,13 +20,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int Delay = 3000;
+        ImageView img = (ImageView)findViewById(R.id.animationView);
+        img.setImageResource(R.drawable.logo);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.common_animation);
+        img.startAnimation(animation);
+
+
+
+        int Delay = 5000;
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent intent = new Intent(MainActivity.this,LoginActivity.class);
                 startActivity(intent);
+                finish();
             }
         },Delay);
     }
